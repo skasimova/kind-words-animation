a5947b90ba33234fb9d2e72f1ad1b607fe393f87
Alex K <contact.alexsophia@gmail.com>
2020-09-28 15:46:44 +0300

    Add adaptive design + fix bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,21 @@ let name = document.getElementById('name');
 
 form.addEventListener('submit', event => {
     event.preventDefault();
-    document.location = "finalpage.html?name-from-previous-page=" + name.value;
+
+    const trimmedName = name.value.trim();
+    const letters = /^[а-яa-z]+$/i;
+
+    if (trimmedName === "") {
+        alert('Please enter something other than spaces ;)');
+        return false;
+    } else if (trimmedName.length > 25) {
+        alert('Please enter a name shorter than 25 letters');
+        return false;
+    } else if (!trimmedName.match(letters)) {
+        alert('Please only enter letters');
+        return false;
+    }
+
+    document.location = "finalpage.html?name-from-previous-page=" + trimmedName;
 });
 
